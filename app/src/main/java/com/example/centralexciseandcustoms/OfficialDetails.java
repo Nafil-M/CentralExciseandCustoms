4b81e1351b7c67daf3e269bcdff1b84489f363be
NAFILM <nafilm26@gmail.com>
2022-04-05 12:51:56 +0530

    Commit 7.

--- app/src/main/java/com/example/centralexciseandcustoms/OfficialDetails.java
+++ app/src/main/java/com/example/centralexciseandcustoms/OfficialDetails.java
@@ -9,16 +9,22 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class OfficialDetails extends AppCompatActivity {
 
-    EditText empid,workplace,dateofj,startdate,enddate,designation;
+    EditText code,email,fname,mname,lname,mobile, dob, doj,dor,education,workexp,
+            hometown,homedist,residaddr,residaddr2,residdist,
+            residpin,residcity,permaddr,permaddr2,permdist,permpin,permcity;
+
+    String codestr,emailstr,fnamestr,mnamestr,lnamestr,mobilestr, dobstr, dojstr,dorstr,educationstr,workexpstr,
+            hometownstr,homediststr,residaddrstr,residaddr2str,residdiststr,
+            residpinstr,residcitystr,permaddrstr,permaddr2str,permdiststr,permpinstr,permcitystr;
+
+    Spinner designation,zone,zonecomm,division,range,category,cadre,disability,residstate,permstate;
     TextView uploadbtn;
-    String id,place,dateofjo,start,end,des;
-//    OfficialDetailsAdapter officialDetailsAdapter;
-//    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,43 +32,106 @@ public class OfficialDetails extends AppCompatActivity {
         setContentView(R.layout.activity_official_details);
 
         uploadbtn = findViewById(R.id.uploadofficial);
-        empid = findViewById(R.id.empidedit);
-        workplace = findViewById(R.id.workplaceedit);
-        dateofj = findViewById(R.id.dateofjedit);
-        startdate = findViewById(R.id.startdateedit);
-        enddate = findViewById(R.id.enddateedit);
-        designation = findViewById(R.id.desigedit);
-//        recyclerView = findViewById(R.id.recyclerview);
-//        officialDetailsAdapter = new OfficialDetailsAdapter(this);
-//
-//        recyclerView.setLayoutManager(new LinearLayoutManager(this,RecyclerView.VERTICAL,false));
-//        recyclerView.setHasFixedSize(true);
-//        recyclerView.setAdapter(officialDetailsAdapter);
+        code = findViewById(R.id.codeedit);
+        email = findViewById(R.id.emailedit);
+        fname = findViewById(R.id.firstnameedit);
+        mname = findViewById(R.id.middlenameedit);
+        lname = findViewById(R.id.lastnameedit);
+        designation = findViewById(R.id.spinnerdesignation);
+        zone = findViewById(R.id.spinnerzone);
+        zonecomm = findViewById(R.id.spinnerzone2);
+        division = findViewById(R.id.spinnerdivision);
+        range = findViewById(R.id.spinnerrange);
+        category = findViewById(R.id.spinnercategory);
+        cadre = findViewById(R.id.spinnercadre);
+        mobile = findViewById(R.id.mobileedit);
+        dob = findViewById(R.id.dobet);
+        doj = findViewById(R.id.dojet);
+        dor = findViewById(R.id.doret);
+        disability = findViewById(R.id.spinnerdisability);
+        education = findViewById(R.id.eduet);
+        workexp = findViewById(R.id.workexpet);
+        hometown = findViewById(R.id.hometet);
+        homedist = findViewById(R.id.hodistet);
+        residaddr = findViewById(R.id.resadd1et);
+        residaddr2 = findViewById(R.id.resadd2et);
+        residdist = findViewById(R.id.residdistet);
+        residpin = findViewById(R.id.residpinet);
+        residcity = findViewById(R.id.residcityet);
+        residstate = findViewById(R.id.spinnerresidstate);
+        permaddr = findViewById(R.id.permenantaddrss1et);
+        permaddr2 = findViewById(R.id.permenantaddrss2et);
+        permdist = findViewById(R.id.permenantdistet);
+        permpin = findViewById(R.id.permenantpinet);
+        permcity = findViewById(R.id.permenantcityet);
+        permstate = findViewById(R.id.spinnerpermenantstate);
 
 
         uploadbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                id = empid.getText().toString();
-                place = workplace.getText().toString();
-                dateofjo = dateofj.getText().toString();
-                start = startdate.getText().toString();
-                end = enddate.getText().toString();
-                des = designation.getText().toString();
+                codestr = code.getText().toString();
+                emailstr = email.getText().toString();
+                fnamestr = fname.getText().toString();
+                mnamestr = mname.getText().toString();
+                lnamestr = lname.getText().toString();
+                mobilestr = mobile.getText().toString();
+                dobstr = dob.getText().toString();
+                dojstr = doj.getText().toString();
+                dorstr = dor.getText().toString();
+                educationstr = education.getText().toString();
+                workexpstr = workexp.getText().toString();
+                hometownstr = hometown.getText().toString();
+                homediststr = homedist.getText().toString();
+                residaddrstr = residaddr.getText().toString();
+                residaddr2str = residaddr2.getText().toString();
+                residdiststr = residdist.getText().toString();
+                residpinstr = residpin.getText().toString();
+                residcitystr = residcity.getText().toString();
+                permaddrstr = permaddr.getText().toString();
+                permaddr2str = permaddr2.getText().toString();
+                permdiststr = permdist.getText().toString();
+                permpinstr = residcity.getText().toString();
+                permcitystr = residcity.getText().toString();
 
+                if (TextUtils.isEmpty(codestr) || TextUtils.isEmpty(emailstr) || TextUtils.isEmpty(fnamestr) || TextUtils.isEmpty(mnamestr) ||
+                TextUtils.isEmpty(lnamestr) || TextUtils.isEmpty(mobilestr) || TextUtils.isEmpty(dobstr) || TextUtils.isEmpty(dojstr) ||
+                        TextUtils.isEmpty(dorstr) || TextUtils.isEmpty(educationstr) || TextUtils.isEmpty(workexpstr) || TextUtils.isEmpty(hometownstr) ||
+                        TextUtils.isEmpty(homediststr) || TextUtils.isEmpty(residaddrstr) || TextUtils.isEmpty(residaddr2str) || TextUtils.isEmpty(residdiststr)
+                        || TextUtils.isEmpty(residpinstr) || TextUtils.isEmpty(residcitystr) || TextUtils.isEmpty(permaddrstr) || TextUtils.isEmpty(permaddr2str)
+                        || TextUtils.isEmpty(permdiststr) || TextUtils.isEmpty(permpinstr) || TextUtils.isEmpty(permcitystr) ) {
 
-                if (TextUtils.isEmpty(id) || TextUtils.isEmpty(place) || TextUtils.isEmpty(dateofjo) || TextUtils.isEmpty(start) || TextUtils.isEmpty(end) || TextUtils.isEmpty(des)) {
                     Toast.makeText(OfficialDetails.this, "All Fields are Important", Toast.LENGTH_SHORT).show();
                 }
                 else {
 
                     Toast.makeText(OfficialDetails.this, "Uploaded Succesfully..!", Toast.LENGTH_SHORT).show();
-                    workplace.setText("");
-                    dateofj.setText("");
-                    startdate.setText("");
-                    enddate.setText("");
-                    designation.setText("");
+
+
+                    code.setText("");
+                    email.setText("");
+                    fname.setText("");
+                    mname.setText("");
+                    lname.setText("");
+                    mobile.setText("");
+                    dob.setText("");
+                    doj.setText("");
+                    dor.setText("");
+                    education.setText("");
+                    workexp.setText("");
+                    hometown.setText("");
+                    homedist.setText("");
+                    residaddr.setText("");
+                    residaddr2.setText("");
+                    residdist.setText("");
+                    residpin.setText("");
+                    residcity.setText("");
+                    permaddr.setText("");
+                    permaddr2.setText("");
+                    permdist.setText("");
+                    permpin.setText("");
+                    permcity.setText("");
 
 //                    Intent intent = new Intent(OfficialDetails.this,MainActivity.class);
 //                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
