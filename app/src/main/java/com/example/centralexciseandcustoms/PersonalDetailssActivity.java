1fb82e6f0b1345b3cb07f4778f5cabe50c3a8b35
NAFILM <nafilm26@gmail.com>
2022-04-11 13:09:32 +0530

    Commit 17.

--- app/src/main/java/com/example/centralexciseandcustoms/PersonalDetailssActivity.java
+++ app/src/main/java/com/example/centralexciseandcustoms/PersonalDetailssActivity.java
@@ -8,7 +8,10 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
@@ -17,6 +20,7 @@ import android.widget.Toast;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 public class PersonalDetailssActivity extends AppCompatActivity {
 
@@ -24,6 +28,7 @@ public class PersonalDetailssActivity extends AppCompatActivity {
     EditText spouseemployement;
     TextView uploadbtn;
     String strspouseemployement;
+    public String maritalstatusselect;
     public static final int RESULT_LOAD_IMG = 100;
 
     @Override
@@ -35,6 +40,40 @@ public class PersonalDetailssActivity extends AppCompatActivity {
         spouseemployed = findViewById(R.id.spinnerspouseemployed);
         spouseemployement = findViewById(R.id.spouseempet);
         uploadbtn = findViewById(R.id.uploadpersonal);
+
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("MARRIED");
+        arrayList.add("SINGLE");
+        arrayList.add("SEPERATED");
+        arrayList.add("DIVORCED");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,                         android.R.layout.simple_spinner_item, arrayList);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        maritalstatus.setAdapter(arrayAdapter);
+        maritalstatus.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                maritalstatusselect = parent.getItemAtPosition(position).toString();
+
+                if (maritalstatusselect.equals("SINGLE") || maritalstatusselect.equals("SEPERATED") || maritalstatusselect.equals("DIVORCED") ){
+                    spouseemployed.setEnabled(false);
+                    spouseemployed.setClickable(false);
+
+                    spouseemployement.setEnabled(false);
+                }else {
+                    spouseemployed.setEnabled(true);
+                    spouseemployed.setClickable(true);
+
+                    spouseemployement.setEnabled(true);
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView <?> parent) {
+            }
+        });
+
+
+
 
         uploadbtn.setOnClickListener(new View.OnClickListener() {
             @Override
